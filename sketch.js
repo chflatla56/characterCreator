ba1b7508fbae2d67602ac7ebdc5e62af3572ed11
chflatla56 <chflatla56@vt.edu>
2024-12-03 13:38:59 -0500

    add base funcs

--- sketch.js
+++ sketch.js
@@ -1,7 +1,82 @@
+let welcomePage = true;
+let dogPage = false;
+let catPage = false;
+let bunnyPage = false;
+let mousePage = false;
+let frogPage = false;
+
+function preload() {
+
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 600);
 }
 
 function draw() {
   background(0);
+
+  if (welcomePage) {
+    drawWelcomePage();
+  } else if (dogPage) {
+    drawDogPage();
+  } else if (catPage) {
+    drawCatPage();
+  } else if (bunnyPage) {
+    drawBunnyPage();
+  } else if (mousePage) {
+    drawMousePage();
+  } else if (frogPage) {
+    drawFrogPage();
+  } else {
+    drawWelcomePage();
+  }
+}
+
+function drawDog() {
+
+}
+
+function drawCat() {
+
+}
+
+function drawBunny() {
+
+}
+
+function drawMouse() {
+
+}
+
+function drawFrog() {
+
+}
+
+function drawWelcomePage() {
+
+}
+
+function drawDogPage(){
+
+}
+
+function drawCatPage() {
+
+}
+
+function drawCatPage() {
+
+}
+
+function drawBunnyPage() {
+
+}
+ 
+function drawMousePage() {
+  
+}
+
+function drawFrogPage() {
+
 }
