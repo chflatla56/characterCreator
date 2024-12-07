c8d26441c04b0ae824808eed88bfc75233623e6b
chflatla56 <chflatla56@vt.edu>
2024-12-07 09:34:10 -0500

    so many edits

--- sketch.js
+++ sketch.js
@@ -29,9 +29,13 @@ let rand2;
 let rand3;
 let rand4;
 
+let hennyPenny;
+
 let click = false;
 
 function preload() {
+  hennyPenny = loadFont("./HennyPenny-Regular.ttf");
+
   dogBase = loadImage("./animals/dog.PNG");
   catBase = loadImage("./animals/cat.PNG");
   bunnyBase = loadImage("./animals/bunny.PNG");
@@ -39,44 +43,40 @@ function preload() {
   frogBase = loadImage("./animals/frog.PNG");
 
   //0, 6, 12, and 18 are the pjs
-
-  dogOptions[0] = loadImage("./dog0.PNG");
-  dogOptions[6] = loadImage("./dog6.PNG");
-  dogOptions[12] = loadImage("./dog12.PNG");
   
-  /*
+  
   for (let i = 0; i <= 5; i++) {
-    dogOptions[i] = loadImage("./dog" + i + ".PNG");
-    catOptions[i] = loadImage("./cat" + i + ".PNG");
-    bunnyOptions[i] = loadImage("./bunny" + i + ".PNG");
-    mouseOptions[i] = loadImage("./mouse" + i + ".PNG");
-    frogOptions[i] = loadImage("./frog" + i + ".PNG");
+    //dogOptions[i] = loadImage("./dog" + i + ".PNG");
+    //catOptions[i] = loadImage("./cat" + i + ".PNG");
+    //bunnyOptions[i] = loadImage("./bunny" + i + ".PNG");
+    mouseOptions[i] = loadImage("./mouse outfits/tops/mouse" + i + ".PNG");
+    //frogOptions[i] = loadImage("./frog" + i + ".PNG");
   }
 
   for (let j = 6; j <= 11; j++) {
-    dogOptions[j] = loadImage("./dog" + j + ".PNG");
-    catOptions[j] = loadImage("./cat" + j + ".PNG");
-    bunnyOptions[j] = loadImage("./bunny" + j + ".PNG");
-    mouseOptions[j] = loadImage("./mouse" + j + ".PNG");
-    frogOptions[j] = loadImage("./frog" + j + ".PNG");
+    //dogOptions[j] = loadImage("./dog" + j + ".PNG");
+    //catOptions[j] = loadImage("./cat" + j + ".PNG");
+    //bunnyOptions[j] = loadImage("./bunny" + j + ".PNG");
+    mouseOptions[j] = loadImage("./mouse outfits/bottoms/mouse" + j + ".PNG");
+    //frogOptions[j] = loadImage("./frog" + j + ".PNG");
   }
 
   for (let k = 12; k <= 17; k++) {
-    dogOptions[k] = loadImage("./dog" + k + ".PNG");
-    catOptions[k] = loadImage("./cat" + k + ".PNG");
-    bunnyOptions[k] = loadImage("./bunny" + k + ".PNG");
-    mouseOptions[k] = loadImage("./mouse" + k + ".PNG");
-    frogOptions[k] = loadImage("./frog" + k + ".PNG");
+    //dogOptions[k] = loadImage("./dog" + k + ".PNG");
+    //catOptions[k] = loadImage("./cat" + k + ".PNG");
+    //bunnyOptions[k] = loadImage("./bunny" + k + ".PNG");
+    mouseOptions[k] = loadImage("./mouse outfits/shoes/mouse" + k + ".PNG");
+    //frogOptions[k] = loadImage("./frog" + k + ".PNG");
   }
 
-  for (let p = 18; p <= 22; p++) {
-    dogOptions[p] = loadImage("./dog" + p + ".PNG");
-    catOptions[p] = loadImage("./cat" + p + ".PNG");
-    bunnyOptions[p] = loadImage("./bunny" + p + ".PNG");
-    mouseOptions[p] = loadImage("./mouse" + p + ".PNG");
-    frogOptions[p] = loadImage("./frog" + p + ".PNG");
+  for (let p = 18; p <= 21; p++) {
+    //dogOptions[p] = loadImage("./dog" + p + ".PNG");
+    //catOptions[p] = loadImage("./cat" + p + ".PNG");
+    //bunnyOptions[p] = loadImage("./bunny" + p + ".PNG");
+    mouseOptions[p] = loadImage("./mouse outfits/accessories/mouse" + p + ".PNG");
+    //frogOptions[p] = loadImage("./frog" + p + ".PNG");
   }
-  */
+  
 }
 
 function setup() {
@@ -96,8 +96,11 @@ function draw() {
     drawWelcomePage();
 
     fill(255);
-    textSize(50);
-    text("five nights at fredder's", 100, 100);
+    textSize(60);
+    textFont(hennyPenny);
+    text("~ fashion friends ~", 350, 125);
+    textSize(40);
+    text("select a character to start!", 375, 760);
 
   } else if (characterPage) {
     if (dogPage) {
@@ -113,7 +116,7 @@ function draw() {
     }
     drawPage();
   } else if (savePage) {
-    //drawSavePage();
+    drawSavePage();
   } else {
     drawWelcomePage();
   }
@@ -148,6 +151,11 @@ function drawWelcomePage() {
   drawMouse(675, 200);
   drawFrog(975, 200);
   pop();
+
+  topCount = 0;
+  bottomCount = 6;
+  shoeCount = 12;
+  accessoryCount = 18;
 }
 
 function drawDogPage(){
@@ -177,15 +185,15 @@ function drawFrogPage() {
 
 function drawOutfit(arr, x, y) {
   if (welcomePage) {
-    image(arr[rand1], x, y, 750, 750);
-    image(arr[rand2], x, y, 750, 750);
-    image(arr[rand3], x, y, 750, 750);
-    image(arr[rand4], x, y, 750, 750);
-  } else if (characterPage) {
+    //image(arr[rand3], x, y, 750, 750);
+    //image(arr[rand2], x, y, 750, 750);
+    //image(arr[rand1], x, y, 750, 750);
+    //image(arr[rand4], x, y, 750, 750);
+  } else if (characterPage || savePage) {
     image(arr[shoeCount], x, y, 750, 750);
-    image(arr[topCount], x, y, 750, 750);
     image(arr[bottomCount], x, y, 750, 750);
-    //image(arr[accessoryCount], x, y, 750, 750);
+    image(arr[topCount], x, y, 750, 750);
+    image(arr[accessoryCount], x, y, 750, 750);
   }
 }
 
@@ -193,31 +201,36 @@ function drawButtons() {
   textSize(30);
   for (let r = 0; r < 600; r = r + 150) {
     fill(255);
-    rect(300, 150 + r, 50, 30);
-    rect(850, 150 + r, 50, 30);
-    fill(150);
-    text("<~", 310, 176 + r);
-    text("~>", 860, 176 + r);
+    rect(300, 150 + r, 50, 30, 90);
+    rect(850, 150 + r, 50, 30, 90);
+    fill(0);
+    text("<", 315, 175 + r);
+    text(">", 867, 175 + r);
   }
 
   fill(255);
-  rect(25, 675, 150, 80);
-  rect(1025, 675, 150, 80);
+  rect(25, 675, 150, 80, 70);
+  rect(1025, 675, 150, 80, 70);
+
+  fill(0);
+  text("home", 65, 730);
+  text("finish", 1060, 730);
 }
 
 function speechBubble() {
   textSize(25);
   fill(255);
-  rect(700, 200, 100, 125);
-  triangle(725, 325, 750, 325, 725, 350);
+  rect(700, 100, 200, 200, 90);
 
   fill(0);
-  rect(725, 280, 50, 25);
+  rect(765, 250, 75, 40, 70);
 
-  text("help", 725, 250);
+  text("Help me pick", 730, 165);
+  text("out an outfit", 725, 200);
+  text("for the day!", 735, 235);
 
   fill(250);
-  text("ok!", 725, 305);
+  text("ok!", 785, 280);
 }
 
 function drawPage() {
@@ -226,6 +239,29 @@ function drawPage() {
   } else {
     speechBubble();
   }
+}
+
+function drawSavePage() {
+
+  if (dogPage) {
+    drawDogPage();
+  } else if (catPage) {
+    drawCatPage();
+  } else if (bunnyPage) {
+    drawBunnyPage();
+  } else if (mousePage) {
+    drawMousePage();
+  } else if (frogPage) {
+    drawFrogPage();
+  }
+
+  fill(255);
+  rect(25, 675, 150, 80, 70);
+  rect(1025, 675, 150, 80, 70);
+
+  fill(0);
+  text("home", 65, 730);
+  text("save", 1070, 725);
 }
 
 function mousePressed() {
@@ -262,70 +298,69 @@ function mousePressed() {
   }
 
   if (characterPage && click) {
-    if (mouseX >= 300 && mouseX <= 350) {
+    if (mouseX >= 850 && mouseX <= 900) {
       if (mouseY >= 150 && mouseY <= 180) {
+        if (accessoryCount < 21) {
+          accessoryCount++;
+        } else if (accessoryCount == 21) {
+          accessoryCount = 19;
+        }
+      } else if (mouseY >= 300 && mouseY <= 330) {
         if (topCount < 5) {
           topCount++;
         } else if (topCount == 5) {
           topCount = 1;
         }
-      } else if (mouseY >= 300 && mouseY <= 330) {
+      } else if (mouseY >= 450 && mouseY <= 480) {
         if (bottomCount < 11) {
           bottomCount++;
         } else if (bottomCount == 11) {
           bottomCount = 7;
         }
-      } else if (mouseY >= 450 && mouseY <= 480) {
+      } else if (mouseY >= 600 && mouseY <= 630) {
         if (shoeCount < 17) {
           shoeCount++;
         } else if (shoeCount == 17) {
           shoeCount = 13;
         }
-      } else if (mouseY >= 600 && mouseY <= 630) {
-        if (accessoryCount < 22) {
-          accessoryCount++;
-        } else if (accessoryCount == 22) {
-          accessoryCount = 19;
-        }
       } 
-    } else if (mouseX >= 800 && mouseX <= 850) {
+    } else if (mouseX >= 300 && mouseX <= 350) {
       if (mouseY >= 150 && mouseY <= 180) {
+        if (accessoryCount > 18) {
+          accessoryCount--;
+        } else if (accessoryCount <= 19) {
+          accessoryCount = 21;
+        }
+      } else if (mouseY >= 300 && mouseY <= 330) { 
         if (topCount > 1) {
           topCount--;
         } else if (topCount <= 1) {
           topCount = 5;
         }
-      } else if (mouseY >= 300 && mouseY <= 330) { 
+      } else if (mouseY >= 450 && mouseY <= 480) {
         if (bottomCount > 7) {
           bottomCount--;
         } else if (bottomCount <= 7) {
           bottomCount = 11;
         }
-      } else if (mouseY >= 450 && mouseY <= 480) {
+      } else if (mouseY >= 600 && mouseY <= 630) {
         if (shoeCount > 13) {
           shoeCount--;
         } else if (shoeCount <= 13) {
           shoeCount = 17;
-        }
-      } else if (mouseY >= 600 && mouseY <= 630) {
-        if (accessoryCount > 19) {
-          accessoryCount--;
-        } else if (accessoryCount <= 19) {
-          accessoryCount = 22;
-        }
+        } 
       }
     }
   }
 
   if (characterPage && !click) {
-    if (mouseX >= 700 && mouseX <= 800 && mouseY >= 200 && mouseY <= 350) {
+    if (mouseX >= 765 && mouseX <= 840 && mouseY >= 250 && mouseY <= 290) {
        click = true;
     }
   }
 
-  if(characterPage) {
-    if (mouseY >= 675 && mouseY <= 755) {
-      if(mouseX >= 25 && mouseX <= 175) {
+  if(characterPage || savePage) {
+    if (mouseY >= 675 && mouseY <= 755 && mouseX >= 25 && mouseX <= 175) {
         welcomePage = true;
          savePage = false;
          characterPage = false;
@@ -335,11 +370,17 @@ function mousePressed() {
          mousePage = false;
          frogPage = false;
          click = false;
-      } else if (mouseX >= 1025 && mouseX <= 1175) {
-         welcomePage = false;
-         savePage = true;
-         characterPage = false;
-      }
+    
+    }
+  }
+
+  if (mouseY >= 675 && mouseY <= 755 && mouseX >= 1025 && mouseX <= 1175) {
+    if (characterPage) {  
+      welcomePage = false;
+      savePage = true;
+      characterPage = false;
+    } else if (savePage) {
+      saveCanvas('myCharacter.jpg');
     }
   }
 }
